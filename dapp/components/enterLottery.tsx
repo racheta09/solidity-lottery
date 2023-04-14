2ef21e781b5b5171b8783a00b8ff4198175e104e
racheta <surajshogun@gmail.com>
2023-04-14 16:26:01 +0530

    spelling error

--- dapp/components/enterLottery.tsx
+++ dapp/components/enterLottery.tsx
@@ -337,7 +337,7 @@ export default function EnterLottery({
                                 </th>
                             </tr>
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td className="px-6 py-4">Tickete Cost</td>
+                                <td className="px-6 py-4">Ticket Cost</td>
                                 <th
                                     scope="row"
                                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center"
