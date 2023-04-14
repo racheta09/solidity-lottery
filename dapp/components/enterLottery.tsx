7dc227ed53c0b4ca240e601e088958382b1abb9c
racheta <surajshogun@gmail.com>
2023-04-14 16:49:14 +0530

    ts fix

--- dapp/components/enterLottery.tsx
+++ dapp/components/enterLottery.tsx
@@ -342,7 +342,8 @@ export default function EnterLottery({
                                     scope="row"
                                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center"
                                 >
-                                    {parseInt(registrationAmount)*1e-18} {ticker}
+                                    {parseInt(registrationAmount) * 1e-18}{" "}
+                                    {ticker}
                                 </th>
                             </tr>
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
@@ -364,11 +365,10 @@ export default function EnterLottery({
                     contractAddress={ercContractAddress}
                     contractAbi={erc20Abi}
                     action={(contract) =>
-                        contract.call(
-                            "approve",
+                        contract.call("approve", [
                             lotContractAddress,
-                            registrationAmount
-                        )
+                            registrationAmount,
+                        ])
                     }
                 >
                     Approve{" "}
